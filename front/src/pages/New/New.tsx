6722470de224294cd02ff980dcb9c77cbbbf05f7
stat1202 <hseungjae1202@naver.com>
2023-09-09 22:53:38 +0900

    test-17

--- front/src/pages/New/New.tsx
+++ front/src/pages/New/New.tsx
@@ -6,7 +6,7 @@ function New() {
   //   useRecoilState -> 값, 변경 함수 둘다 사용
   return (
     <div>
-      변경사항 확인-프론트 변경-8
+      변경사항 확인-프론트 변경-9
       <Link to="/test">useRef</Link>
     </div>
   );
