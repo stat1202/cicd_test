d45e2ea1e34811f85e05204fb14882021400dd39
stat1202 <hseungjae1202@naver.com>
2023-09-10 14:31:49 +0900

    test-32

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>,
 );
